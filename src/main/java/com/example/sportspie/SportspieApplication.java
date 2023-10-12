d8f3d999c91f0252fe7a5ab04315a6597a67f5c0
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-12 22:48:20 +0900

    chore: Spring 프로젝트 초기 설정

---
+++ src/main/java/com/example/sportspie/SportspieApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportspie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportspieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportspieApplication.class, args);
+	}
+
+}
