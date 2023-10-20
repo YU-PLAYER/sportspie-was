973a98336f16f11ee3b7df01442163a2950d5b44
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-20 16:16:12 +0900

    feat: position enum 정의

---
+++ src/main/java/com/example/sportspie/auth/type/Position.java
@@ -0,0 +1,16 @@
+package com.example.sportspie.auth.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Position {
+	GK(0, "골키퍼"),
+	DF(1, "수비수"),
+	MF(2, "미드필더"),
+	FW(3, "공격수");
+
+	private int id;
+	private String name;
+}
