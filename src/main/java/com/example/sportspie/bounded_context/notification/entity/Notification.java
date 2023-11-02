cbe27328ced8207199b9bb7dcd0d8e2069461574
pingu99 <nurugji@gmail.com>
2023-11-03 03:08:06 +0900

    fix: notification 도메인 추가, entity builder 추가

--- src/main/java/com/example/sportspie/bounded_context/notification/entity/Notification.java
+++ src/main/java/com/example/sportspie/bounded_context/notification/entity/Notification.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,4 +27,24 @@ public class Notification extends BaseTimeEntity {
 
 	@Column(columnDefinition = "TINYINT", nullable = false)
 	private NotificationType type;
+
+	@Builder
+	public Notification(User receiver, NotificationType type) {
+		this.receiver = receiver;
+		this.type = type;
+		switch (type){
+			case GAME_CONFIRMED:
+               	this.content = "경기가 확정되었습니다.";
+                break;
+            case RESULTS_CONFIRMED:
+                this.content = "경기결과가 확정되었습니다.";
+                break;
+            case DATE_IMMINENT:
+                this.content = "경기 시작이 임박했습니다.";
+                break;
+            case REPORTED:
+                this.content = "신고가 완료되었습니다.";
+                break;
+		}
+	}
 }
