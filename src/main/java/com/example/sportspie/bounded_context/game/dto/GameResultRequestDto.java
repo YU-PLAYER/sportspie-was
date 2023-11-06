a172d76377fdcc791679ebf2d802d50bea06dea1
pingu99 <nurugji@gmail.com>
2023-11-06 19:35:52 +0900

    feat: Game result dto 추가

---
+++ src/main/java/com/example/sportspie/bounded_context/game/dto/GameResultRequestDto.java
@@ -0,0 +1,21 @@
+package com.example.sportspie.bounded_context.game.dto;
+
+import com.example.sportspie.bounded_context.game.type.GameResult;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GameResultRequestDto {
+    private Long userId;
+    private Long gameId;
+    private GameResult gameResult;
+
+    @Builder
+    public GameResultRequestDto(Long userId, Long gameId, GameResult gameResult) {
+        this.userId = userId;
+        this.gameId = gameId;
+        this.gameResult = gameResult;
+    }
+}
