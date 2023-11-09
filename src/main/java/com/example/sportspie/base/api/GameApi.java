b11b2e581c6ad2eb27b763d3bf3735865bfa1f3c
pingu99 <nurugji@gmail.com>
2023-11-09 23:48:03 +0900

    fix: Game API 경기 목록 조회 페이징, 검색 기능 추가 Delete 메서드 DELETE → POST

--- src/main/java/com/example/sportspie/base/api/GameApi.java
+++ src/main/java/com/example/sportspie/base/api/GameApi.java
@@ -1,13 +1,18 @@
 package com.example.sportspie.base.api;
 
+import com.example.sportspie.bounded_context.game.dto.GameListResponseDto;
 import com.example.sportspie.bounded_context.game.dto.GameResultRequestDto;
-import com.example.sportspie.bounded_context.game.dto.GameUserInfoRequestDto;
+import com.example.sportspie.bounded_context.game.dto.GameUserRequestDto;
 import com.example.sportspie.bounded_context.game.dto.GameRequestDto;
 import com.example.sportspie.bounded_context.game.entity.Game;
 import com.example.sportspie.bounded_context.game.type.GameStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +28,14 @@ public interface GameApi {
     Game create(@RequestBody GameRequestDto gameRequestDto);
 
     @GetMapping("/{startedAt}")
-    @Operation(summary = "Game 목록 조회 메서드", description = "사용자가 경기 목록을 조회하기 위한 메서드 입니다.")
-    List<Game> list(@DateTimeFormat(pattern = "yyyy-MM-dd") @PathVariable LocalDate startedAt);
+    @Operation(summary = "Game 날짜별 목록 조회 메서드", description = "사용자가 시스템의 모든 경기 목록을 날짜별로 조회하기 위한 메서드 입니다.")
+    Page<GameListResponseDto> list(@DateTimeFormat(pattern = "yyyy-MM-dd") @PathVariable LocalDate startedAt,
+                                   @RequestParam(name = "sortBy", defaultValue = "ASC")String sort,
+                                   @PageableDefault(page = 0, size = 10) Pageable pageable);
+    @GetMapping("/search")
+    @Operation(summary = "Game 제목 검색 메서드", description = "사용자가 시스템의 모든 경기 목록에 대해 제목으로 검색하기 위한 메서드 입니다.")
+    Page<GameListResponseDto> list(@RequestParam(name = "title", defaultValue = "")String title,
+                                   @PageableDefault(page = 0, size = 10) Pageable pageable);
 
     @GetMapping("/detail/{id}")
     @Operation(summary = "Game 상세 조회 메서드", description = "로그인한 사용자가 경기 정보를 상세 조회하기 위한 메서드 입니다.")
@@ -32,13 +43,13 @@ public interface GameApi {
 
     @PatchMapping("/progress")
     @Operation(summary = "Game 인원 확정 메서드", description = "경기 인원 모집글 작성자가 경기 인원 확정 조건을 만족할 때 경기 인원을 확정하기 위한 메서드 입니다.")
-    Game personConfirm(@RequestBody GameUserInfoRequestDto gameUserInfoRequestDto);
+    Game personConfirm(@RequestBody GameUserRequestDto gameUserRequestDto);
 
     @PatchMapping("/after")
     @Operation(summary = "Game 결과 확정 메서드", description = "경기 인원 모집글 작성자가 경기 결과 확정 조건을 만족할 때 경기 결과를 확정하기 위한 메서드 입니다.")
     Game resultConfirm(@RequestBody GameResultRequestDto gameResultRequestDto);
 
-    @DeleteMapping("/delete")
+    @PostMapping("/delete")
     @Operation(summary = "Game 삭제 메서드", description = "경기 인원 모집글 작성자가 경기 삭제 조건을 만족할 때 경기를 삭제하기 위한 메서드 입니다.")
-    Game delete(@RequestBody GameUserInfoRequestDto gameUserInfoRequestDtog);
+    Game delete(@RequestBody GameUserRequestDto gameUserRequestDto);
 }
