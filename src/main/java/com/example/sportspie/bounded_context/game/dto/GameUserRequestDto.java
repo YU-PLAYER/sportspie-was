f975b29f7c62d2e52209dea9a9350e9c65ced400
pingu99 <nurugji@gmail.com>
2023-11-09 23:46:45 +0900

    fix : Game dto 추가, 수정

--- src/main/java/com/example/sportspie/bounded_context/game/dto/GameUserRequestDto.java
+++ src/main/java/com/example/sportspie/bounded_context/game/dto/GameUserRequestDto.java
@@ -1,17 +1,15 @@
 package com.example.sportspie.bounded_context.game.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class GameUserInfoRequestDto {
+public class GameUserRequestDto {
     private Long userId;
     private Long gameId;
 
-    @Builder
-    public GameUserInfoRequestDto(Long userId, Long gameId) {
+    public GameUserRequestDto(Long userId, Long gameId) {
         this.userId = userId;
         this.gameId = gameId;
     }
