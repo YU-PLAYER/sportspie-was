1a38ae8eee255f2548b9776d4af782d8ce102bba
pingu99 <nurugji@gmail.com>
2023-11-09 23:49:58 +0900

    fix: Game repository 검색 기능 추가

--- src/main/java/com/example/sportspie/bounded_context/game/repository/GameRepository.java
+++ src/main/java/com/example/sportspie/bounded_context/game/repository/GameRepository.java
@@ -1,11 +1,14 @@
 package com.example.sportspie.bounded_context.game.repository;
 
 import com.example.sportspie.bounded_context.game.entity.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.time.LocalDateTime;
-import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    List<Game> findByStartedAtBetween(LocalDateTime start, LocalDateTime end);
+    Page<Game> findByStartedAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+    Page<Game> findByTitleContaining(String keyword, Pageable pageable);
 }
