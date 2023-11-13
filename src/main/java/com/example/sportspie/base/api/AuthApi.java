cfefb4d5413e4099700446eab8c041e41bb2e801
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-14 00:22:19 +0900

    fix: OAuth 로그인 API method 변경

--- src/main/java/com/example/sportspie/base/api/AuthApi.java
+++ src/main/java/com/example/sportspie/base/api/AuthApi.java
@@ -3,6 +3,7 @@ package com.example.sportspie.base.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,7 +16,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/api/auth")
 @Tag(name = "Auth", description = "사용자 인증 관련 API")
 public interface AuthApi {
-	@GetMapping("/sign-in/{platform}")
+	@PostMapping("/sign-in/{platform}")
 	@Operation(summary = "OAuth 로그인 메서드", description = "사용자가 OAuth 로그인을 하기 위한 메서드입니다.")
 	ResponseEntity<JwtDto> signIn(@PathVariable String platform, @RequestBody OAuthTokenRequestDto oAuthTokenDto);
 }
