49e2732ea298ee193fde0fd0f050976184c12cfc
HyeonjunOh <znzn2260@naver.com>
2023-11-17 18:54:34 +0900

    feat: 날씨 업데이트 쿼리 추가 #38

--- src/main/java/com/example/sportspie/bounded_context/stadium/repository/StadiumRepository.java
+++ src/main/java/com/example/sportspie/bounded_context/stadium/repository/StadiumRepository.java
@@ -3,6 +3,7 @@ package com.example.sportspie.bounded_context.stadium.repository;
 import com.example.sportspie.bounded_context.stadium.dto.INearbyStadium;
 import com.example.sportspie.bounded_context.stadium.entity.Stadium;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,4 +19,12 @@ s.longitude AS longitude,
 FROM stadium AS s HAVING distance < 10 ORDER BY distance
 """, nativeQuery = true)
     List<INearbyStadium> findNearbyStadium(@Param(value="latitude") Double latitude, @Param(value="longitude") Double longitude);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+UPDATE stadium s
+SET s.weather_type = :weatherType
+where s.id = :id
+""", nativeQuery = true)
+    void updateWeatherType(@Param(value = "id") Long id, @Param(value = "weatherType") int weatherType);
 }
