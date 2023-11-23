ebcdcc33c14dfc181ca7267385b9862f9107eef7
pingu99 <nurugji@gmail.com>
2023-11-23 21:40:05 +0900

    refactor: userId > User 객체 사용

--- src/main/java/com/example/sportspie/bounded_context/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/sportspie/bounded_context/notification/repository/NotificationRepository.java
@@ -1,10 +1,14 @@
 package com.example.sportspie.bounded_context.notification.repository;
 
+import com.example.sportspie.bounded_context.auth.entity.User;
 import com.example.sportspie.bounded_context.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByReceiverId(Long receiverId);
+    List<Notification> findByReceiverOrderByCreatedAt(User receiver);
 }
+
+
+
