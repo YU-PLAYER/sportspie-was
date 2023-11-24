7a8f42c3c238797eebde8101130524764897d386
pingu99 <nurugji@gmail.com>
2023-11-24 16:47:13 +0900

    fix: Security gameUser 추가

--- src/main/java/com/example/sportspie/base/security/config/SecurityConfig.java
+++ src/main/java/com/example/sportspie/base/security/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
 										"/api/notice/**",
 										"/api/notification/**",
 										"/api/stadium/**",
+										"/api/gameUser/**",
 										"/api/game/**").permitAll()
 								.anyRequest().authenticated())
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
