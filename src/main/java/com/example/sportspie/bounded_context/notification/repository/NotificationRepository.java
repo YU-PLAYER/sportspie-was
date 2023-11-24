1ab62f8b902e5966cff72c193ee873d674f5b726
pingu99 <nurugji@gmail.com>
2023-11-24 18:58:23 +0900

    fix: list create_at DESC로 정렬 수정

--- src/main/java/com/example/sportspie/bounded_context/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/sportspie/bounded_context/notification/repository/NotificationRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByReceiverOrderByCreatedAt(User receiver);
+    List<Notification> findByReceiverOrderByCreatedAtDesc(User receiver);
 }
 
 
