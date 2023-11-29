93e263799da54a22044d0d5564d80ebd2d391d3c
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-30 04:08:51 +0900

    feat: 이미지 url 반환 dto 정의

---
+++ src/main/java/com/example/sportspie/bounded_context/auth/dto/ImageUrlResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.sportspie.bounded_context.auth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ImageUrlResponseDto {
+	private String url;
+}
