a98f6f61d8b449cc969bf1882b475e4eb716a408
pingu99 <nurugji@gmail.com>
2023-11-30 21:33:46 +0900

    fix: 알림관련 API HttpServletRequest 추가

--- src/main/java/com/example/sportspie/base/api/NotificationApi.java
+++ src/main/java/com/example/sportspie/base/api/NotificationApi.java
@@ -5,6 +5,7 @@ import com.example.sportspie.bounded_context.notification.dto.NotificationRespon
 import com.example.sportspie.bounded_context.notification.entity.Notification;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +14,11 @@ import java.util.List;
 @RequestMapping("/api/notification")
 @Tag(name = "Notification", description = "알림 관련 API")
 public interface NotificationApi {
-    @GetMapping("/{receiverId}")
+    @GetMapping("")
     @Operation(summary = "알림 목록 조회 메서드", description = "사용자의 알림 목록을 조회하기 위한 메서드입니다.")
-    List<NotificationResponseDto> list(@PathVariable Long receiverId);
+    List<NotificationResponseDto> list(HttpServletRequest request);
 
     @DeleteMapping("/{id}")
     @Operation(summary = "알림 삭제 메서드", description = "사용자의 알림을 삭제하기 위한 메서드입니다.")
-    ResponseEntity<StateResponse> delete(@PathVariable Long id);
+    ResponseEntity<StateResponse> delete(@PathVariable Long id, HttpServletRequest request);
 }
