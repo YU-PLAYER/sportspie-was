8372cc8b3de76e0d2fcb4f213abbf9567e36befb
pingu99 <nurugji@gmail.com>
2023-11-30 21:34:15 +0900

    fix: 알림관련 API JWTProvider 사용

--- src/main/java/com/example/sportspie/bounded_context/notification/controller/NotificationController.java
+++ src/main/java/com/example/sportspie/bounded_context/notification/controller/NotificationController.java
@@ -2,9 +2,11 @@ package com.example.sportspie.bounded_context.notification.controller;
 
 import com.example.sportspie.base.api.NotificationApi;
 import com.example.sportspie.base.error.StateResponse;
+import com.example.sportspie.base.jwt.util.JwtProvider;
 import com.example.sportspie.bounded_context.notification.dto.NotificationResponseDto;
 import com.example.sportspie.bounded_context.notification.entity.Notification;
 import com.example.sportspie.bounded_context.notification.service.NotificationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +17,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationController implements NotificationApi {
     private final NotificationService notificationService;
+    private final JwtProvider jwtProvider;
 
     @Override
-    public List<NotificationResponseDto> list(Long receiverId) {
-        return notificationService.list(receiverId);
+    public List<NotificationResponseDto> list(HttpServletRequest request) {
+        Long userId = jwtProvider.getUserId(jwtProvider.resolveToken(request).substring(7));
+        return notificationService.list(userId);
     }
 
     @Override
-    public ResponseEntity<StateResponse> delete(Long id) {
-        return notificationService.delete(id);
+    public ResponseEntity<StateResponse> delete(Long id, HttpServletRequest request) {
+        Long userId = jwtProvider.getUserId(jwtProvider.resolveToken(request).substring(7));
+        return notificationService.delete(userId, id);
     }
 }
