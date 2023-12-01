0cb76ec40adf4b4aa3a6ac2b3da7bfe4c06d49bc
pingu99 <nurugji@gmail.com>
2023-12-01 13:58:21 +0900

    fix: Game delete API 사용시 GameUser delete casecade 추가

--- src/main/java/com/example/sportspie/bounded_context/game/service/GameService.java
+++ src/main/java/com/example/sportspie/bounded_context/game/service/GameService.java
@@ -25,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -138,6 +139,8 @@ public class GameService{
     public ResponseEntity<StateResponse> delete(Long userId, Long gameId){
         User user = userService.read(userId);
         Game game = read(gameId);
+        List<GameUser> gameUsers = gameUserRepository.findGameUserByJoinGame(game);
+        gameUserRepository.deleteAll(gameUsers);
 
         if(!game.isAuthor(user)) throw new IllegalArgumentException("작성자만 경기를 삭제할 수 있습니다.");
         if(!game.isSatisfiedDelete()) throw new IllegalArgumentException("경기 인원이 확정되지 않았고 경기 시작 시간 2시간 전인 경기만 삭제할 수 있습니다.");
